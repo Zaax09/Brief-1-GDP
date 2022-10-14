2d024a0d6a835b423a16aee9ee8c5e8831fd61b5
Zakaria09 <92021986+Zaax09@users.noreply.github.com>
2022-10-14 13:48:12 +0100

    Done 2

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//  // supprimer function
+//  public function deleteID($delid){
+//     $sql = "DELETE FROM `promotion` WHERE id =$delid";
+//     $result = mysqli_query($this->Connect(), $sql);
+
+// }
+
+
+
+
+
+
+
+
+?>
